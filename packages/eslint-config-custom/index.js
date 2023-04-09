b6030ca16dae1f4a9ad96f22d3b7be7b369da4fb
Gen Ichino <zenoplex@gmail.com>
2023-04-09 20:15:54 +0900

    Initial commit

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,44 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:functional/external-recommended',
+    'plugin:functional/no-mutations',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'next',
+    'turbo',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'functional', 'import'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
+    // Disable readonly since its tedious
+    '@typescript-eslint/prefer-readonly': 'off',
+    '@typescript-eslint/prefer-readonly-parameter-types': 'off',
+    'functional/prefer-readonly-type': 'off',
+    // No any
+    '@typescript-eslint/no-explicit-any': 'error',
+    // Disable immutable data
+    'functional/immutable-data': [
+      'error',
+      {
+        // React ref is mutable
+        ignoreAccessorPattern: ['ref.current*.**', '**Ref.current*.**'],
+        ignorePattern: ['document.title', 'module.exports'],
+      },
+    ],
+    // Auto order imports
+    'import/order': 'error',
+    // Default from turborepo
+    '@next/next/no-html-link-for-pages': 'off',
   },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
-    },
-  },
+  // This causes problem with linting in VSCode.
+  // We may need to use this in the future.
+  // * See: https://github.com/vercel/next.js/issues/40687
+  // parserOptions: {
+  //   babelOptions: {
+  //     presets: [require.resolve('next/babel')],
+  //   },
+  // },
 };
