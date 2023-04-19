247fedeb9934df966ce9a29a0ca889771596531b
Gen Ichino <zenoplex@gmail.com>
2023-04-19 21:36:18 +0900

    style: run format
    npm run format

--- apps/workshop/.storybook/main.ts
+++ apps/workshop/.storybook/main.ts
@@ -1,14 +1,18 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: {
     name: '@storybook/react-vite',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: 'tag'
+    autodocs: 'tag',
   },
-  staticDirs: ['../public']
+  staticDirs: ['../public'],
 };
 export default config;
