fb2355c3523d708da29e1e058ffa956af9b16cea
Gen Ichino <zenoplex@gmail.com>
2023-05-01 23:12:48 +0900

    docs: update comment

--- .circleci/regsuit_gh_status.sh
+++ .circleci/regsuit_gh_status.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -eu
 
+# Reg generated file
 regOutput='.reg/out.json'
 regOutputCat=$(cat $regOutput)
 
@@ -11,6 +12,7 @@ deleted_count="$(echo "$regOutputCat" | jq '.deletedItems | length')"
 # Text to display to gh status 
 description="fail: $failed_count, new: $new_count, del: $deleted_count, pass: $passed_count"
 
+# Extract repo_owner and repo_name from CIRCLE_REPOSITORY_URL(git@github.com:<repo_owner>/<repo_name>.git)
 repo_owner=$(echo "$CIRCLE_REPOSITORY_URL" | awk -F '[:/.]' '{print $3}')
 repo_name=$(echo "$CIRCLE_REPOSITORY_URL" | awk -F '[:/.]' '{print $4}')
 
