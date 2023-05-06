8cfb920f9d3fd33e213a06f25d4adcbf2dbba1e0
Gen Ichino <zenoplex@gmail.com>
2023-05-06 22:34:15 +0900

    style: remove eslint disable

--- packages/use-pagination/src/utils/createRange.test.ts
+++ packages/use-pagination/src/utils/createRange.test.ts
@@ -3,21 +3,26 @@ import { createRange } from './createRange';
 
 describe('getRange', () => {
   test.each<[[start: number, end: number], number[]]>([
-    /* eslint-disable prettier/prettier */
-    [[1, 3],[1, 2, 3]],
-    [[5, 10],[5, 6, 7, 8, 9, 10]],
-    [[-1, 3],[-1, 0, 1, 2, 3]],
+    [
+      [1, 3],
+      [1, 2, 3],
+    ],
+    [
+      [5, 10],
+      [5, 6, 7, 8, 9, 10],
+    ],
+    [
+      [-1, 3],
+      [-1, 0, 1, 2, 3],
+    ],
     [[1, 1], [1]],
-    /* eslint-enable prettier/prettier */
   ])('should return an array of numbers', (input, expected) => {
     expect(createRange(...input)).toEqual(expected);
   });
 
   test.each<[[start: number, end: number], number[]]>([
-    /* eslint-disable prettier/prettier */
-    [[3, 2],[]],
-    [[3, -3],[]],
-    /* eslint-enable prettier/prettier */
+    [[3, 2], []],
+    [[3, -3], []],
   ])('should return an empty array of numbers', (input, expected) => {
     expect(createRange(...input)).toEqual(expected);
   });
