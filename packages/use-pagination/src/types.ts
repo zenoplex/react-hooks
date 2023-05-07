721ddc3a01c8f7a13b84c05bc7b9965a5e40e89f
Gen Ichino <zenoplex@gmail.com>
2023-05-08 00:04:33 +0900

    feat: add id to entities

--- packages/use-pagination/src/types.ts
+++ packages/use-pagination/src/types.ts
@@ -14,36 +14,42 @@ export type Entity =
   | LastEntity;
 
 export type PageEntity = {
+  id: string;
   type: Page;
   value: number;
   active: boolean;
 };
 
 export type EllipsisEntity = {
+  id: 'left' | 'right';
   type: Ellipsis;
   value: null;
   active: false;
 };
 
 export type NextEntity = {
+  id: 'next';
   type: Next;
   value: number;
   active: boolean;
 };
 
 export type PreviousEntity = {
+  id: 'previous';
   type: Previous;
   value: number;
   active: boolean;
 };
 
 export type FirstEntity = {
+  id: 'first';
   type: First;
   value: number;
   active: boolean;
 };
 
 export type LastEntity = {
+  id: 'last';
   type: Last;
   value: number;
   active: boolean;
