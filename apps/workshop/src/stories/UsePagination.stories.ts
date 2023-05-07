e53d2207b99166e0a08a9431112589ead9679dc9
Gen Ichino <zenoplex@gmail.com>
2023-05-07 12:19:10 +0900

    test: add story

---
+++ apps/workshop/src/stories/UsePagination.stories.ts
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+import { UsePagination } from './UsePagination';
+
+const meta = {
+  component: UsePagination,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  args: {
+    currentPage: 1,
+    totalPages: 5,
+  },
+} satisfies Meta<typeof UsePagination>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    expect(canvas.getAllByText(/\d/)).toHaveLength(5);
+  },
+};
+
+export const Ellipsis: Story = {
+  args: {
+    currentPage: 12,
+    totalPages: 30,
+  },
+};
+
+export const ShowPreviousAndNext: Story = {
+  args: {
+    currentPage: 12,
+    totalPages: 30,
+    showPreviousAndNext: true,
+  },
+};
+
+export const ShowFirstAndLast: Story = {
+  args: {
+    currentPage: 12,
+    totalPages: 30,
+    showFirstAndLast: true,
+  },
+};
