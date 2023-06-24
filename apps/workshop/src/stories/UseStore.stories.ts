2d0aed5a1b664d5a3176b06b520a6ca158a72160
Gen Ichino <zenoplex@gmail.com>
2023-06-24 23:30:13 +0900

    test: add story

---
+++ apps/workshop/src/stories/UseStore.stories.ts
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+import { sleep } from './sleep';
+import { UseStore } from './UseStore';
+
+const meta = {
+  component: UseStore,
+} satisfies Meta<typeof UseStore>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  play: async ({ canvasElement, step }) => {
+    await sleep();
+    const canvas = within(canvasElement);
+
+    expect(canvas.getByTestId('global-count').textContent).toBe(
+      'Global count: 0'
+    );
+    expect(canvas.getByTestId('other-count').textContent).toBe(
+      'Other count: 0'
+    );
+
+    await step('increment other', async () => {
+      await userEvent.click(canvas.getByText('load other 2'));
+      expect(canvas.getByTestId('other-count').textContent).toBe(
+        'Other count: 1'
+      );
+
+      await userEvent.click(canvas.getByText('load other 2'));
+      expect(canvas.getByTestId('other-count').textContent).toBe(
+        'Other count: 2'
+      );
+    });
+
+    await step('increment global', async () => {
+      await userEvent.click(canvas.getByText('load global 2'));
+      expect(canvas.getByTestId('global-count').textContent).toBe(
+        'Global count: 1'
+      );
+
+      await userEvent.click(canvas.getByText('load global 2'));
+      expect(canvas.getByTestId('global-count').textContent).toBe(
+        'Global count: 2'
+      );
+    });
+  },
+};
