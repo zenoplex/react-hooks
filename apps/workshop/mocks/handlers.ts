890e0d1a31d3fe5b7d269e70d00967a76272cb50
Gen Ichino <zenoplex@gmail.com>
2023-08-23 08:52:31 +0900

    chore: update response

--- apps/workshop/mocks/handlers.ts
+++ apps/workshop/mocks/handlers.ts
@@ -4,8 +4,8 @@ export const handlers = [
   rest.get('/user', (req, res, ctx) => {
     return res(
       ctx.json({
-        firstName: 'Neil',
-        lastName: 'Maverick',
+        firstName: 'Neil_2',
+        lastName: 'Maverick_2',
       })
     );
   }),
