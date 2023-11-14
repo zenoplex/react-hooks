00e2ca1d0be78b52a59b2e6bc9867116cb9bff28
Gen Ichino <zenoplex@gmail.com>
2023-11-15 01:54:01 +0900

    test: Update callback

--- apps/workshop/src/stories/UseClickOutside.tsx
+++ apps/workshop/src/stories/UseClickOutside.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import useClickOutside from '@gen/use-click-outside';
 
 export const UseClickOutside: React.FC = () => {
-  const [setRef] = useClickOutside({
-    onClickOutside: () => window.alert('clicked outside'),
-  });
+  const [setRef] = useClickOutside(
+    () => window.alert('clicked outside'),
+  );
 
   return (
     <div className="bg-slate-300 p-3 flex flex-col gap-3">
